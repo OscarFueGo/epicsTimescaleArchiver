86769a2a5f87761561d45542a03b1c668358917a
Oscar Fuentes Godoy <ofugo95@gmail.com>
2024-12-16 22:38:37 -0300

    feat GET to archive

--- engine/server.py
+++ engine/server.py
@@ -1,25 +1,52 @@
 from flask import Flask, jsonify, request
 from waitress import serve
 import subprocess
+import sys 
+from flask_cors import CORS
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logging.info("Server started...")
 
 app = Flask(__name__)
 
-def executeCommand(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+CORS(app)
+
+processList = []
+class ProcessManager:
+    def __init__(self):
+        self.processes = {}
+    def activateArchiving(self, channel):
+        process = subprocess.Popen(["python3","./engine.py",channel,"1"])
+        self.processes[channel] =process
+    def deactivateArchiving(self, channel):
+        process = self.processes.get(channel)   
+        if process:
+            print(f"Stopping process for {channel} with PID {process.pid}")
+            process.terminate()  # Graceful termination
+            process.wait()
+            del self.processes[channel]
+            print(f"Process for {channel} stopped.")
+        else:
+            print(f"No process found for {channel}")
 
 @app.route("/", methods=['GET'])
 def root():
+    logging.info("home...")
     return "Hi from server!"
+
+
+@app.route("/stopArchive", methods=['GET'])
+def stopArchive():
+    channel = request.args.get('channel')
+    manager.deactivateArchiving(channel)
 
 @app.route("/archive", methods=['GET'])
 def archive():
-    data = request.get_json()  # Parse incoming JSON data
-    if not data:
-        return jsonify({"error": "No data provided"}), 400
-    # Process the data (e.g., store it or return a response)
-    return jsonify({"message": "Data received successfully", "data": data}), 200
+    channel = request.args.get('channel')
+    manager.activateArchiving(channel)
 
 if __name__ == '__main__':
-    executeCommand(["python3","./engine.py","softIoc:heartbeat","1"])
+    manager = ProcessManager()
     serve(app, host='0.0.0.0', port=8000, threads=50)
+    sys.stdout.flush()
